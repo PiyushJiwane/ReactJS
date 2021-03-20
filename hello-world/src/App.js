37724403a1d0570359f5b718b85fb55b7b9e3a72
PiyushMauli <jiwanepiyush@gmail.com>
2021-03-20 14:27:05 +0530

    Functional Component in ReactJS

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './Components/Greet';
 
 function App() {
+
+  // first hello-world program in ReactJS
+  // =========================================
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         <code>Hello Piyush Jiwane</code>
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
+  // functional components in ReactJs
+  // ===================================
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello Piyush Jiwane</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <Greet></Greet>
+      </div>
+    );
 }
 
 export default App;
