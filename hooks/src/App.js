3cebc12f3f541b8e5640ee80dc4ec01366372cd6
PiyushMauli <jiwanepiyush@gmail.com>
2021-03-27 15:10:34 +0530

    useState() with object

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import CounterHook from './Components/CounterHook';
+import CounterPreStateHook from './Components/CounterPreStateHook';
+import UseStateObjectHook from './Components/UseStateObjectHook';
 
 function App() {
   return (
     <div className="App">
-      <CounterHook/>
+      {/* <CounterHook/> */}
+      {/* <CounterPreStateHook/> */}
+      <UseStateObjectHook/>
     </div>
   );
 }
