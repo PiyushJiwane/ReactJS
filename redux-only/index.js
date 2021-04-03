26ea05c79add5d48fa0bb6aea006455bd717f7d4
PiyushMauli <jiwanepiyush@gmail.com>
2021-04-03 10:10:43 +0530

    understand how to work with the seprate reducers and combine them later by using combineReducers() method.

--- redux-only/index.js
+++ redux-only/index.js
@@ -1,50 +1,90 @@
-const redux=require('redux');
-const createStore=redux.createStore;
+const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducer=redux.combineReducers;
 
-const  BUY_CAKE="BUY_CAKE";
-const BUY_ICECRAM="BUY_ICECREAM";
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECRAM = "BUY_ICECREAM";
 
-function buyCake(){
-    return {
-        type:BUY_CAKE,
-        info:"First Redux Ex"
-    }
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First Redux Ex",
+  };
 }
-function buyIceCream(){
-    return {
-        type:BUY_ICECRAM,
-    }
-}
-
-const initiatlState={
-    numOfCakes:10,
-    numOfIceCream:20
+function buyIceCream() {
+  return {
+    type: BUY_ICECRAM,
+  };
 }
 
-const reducer=(state=initiatlState,action)=>{
-    switch(action.type){
-        case BUY_CAKE:
-            return {
-                ... state,
-                numOfCakes:state.numOfCakes-1
-            }
-        case BUY_ICECRAM:
-            return {
-                ... state,
-                numOfIceCream:state.numOfIceCream-1
-            }
-        default:
-            return state
-    }
-}
+// const initiatlState={
+//     numOfCakes:10,
+//     numOfIceCream:20
+// }
 
-const store=createStore(reducer);
+const initialCakeState = {
+  numOfCakes: 10,
+};
 
-console.log("initial state",store.getState());
-const unsubcribe=store.subscribe(()=>console.log("updated state",store.getState()));
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyIceCream())
-store.dispatch(buyIceCream())
+const initialIceCreamState = {
+  numOfIceCream: 20,
+};
+
+// const reducer = (state = initiatlState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state,
+//         numOfCakes: state.numOfCakes - 1,
+//       };
+//     case BUY_ICECRAM:
+//       return {
+//         ...state,
+//         numOfIceCream: state.numOfIceCream - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECRAM:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer=combineReducer({
+    cake:cakeReducer,
+    iceCream:iceCreamReducer
+})
+
+const store = createStore(rootReducer);
+
+console.log("initial state", store.getState());
+const unsubcribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubcribe();
